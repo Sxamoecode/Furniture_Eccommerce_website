c86f06bc3881af55a3ad9f94dff1658681aaee31
baay02 <baaybilya25@yahoo.com>
2023-09-22 20:58:38 +0100

    update

--- src/app.js
+++ src/app.js
@@ -10,7 +10,7 @@ const app = express();
 
 app.use(morgan('dev'));
 app.use(cors({
-    origin: 'http://localhost:7000'
+    origin: '*'
  }));
  app.use(bodyParser.json());
  app.use(express.urlencoded({
